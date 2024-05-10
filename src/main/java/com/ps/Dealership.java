e951856b55dc5f34ecf739fcdd9726cb9a74bdc6
Jeremy <ogprago@gmail.com>
2024-05-10 11:55:35 -0400

    Created Vehicle, DealershipFileManager, Dealership, and UserInterface classes. Created inventory.txt to story vehilcle inventory and info

---
+++ src/main/java/com/ps/Dealership.java
@@ -0,0 +1,61 @@
+package com.ps;
+
+import java.util.ArrayList;
+
+public class Dealership {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    ArrayList<Vehicle> vehicles = new ArrayList<>();
+
+    public Dealership(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    private ArrayList<Vehicle> getAllVehicles() {
+        return null;
+    }
+
+    private ArrayList<Vehicle> getVehiclesByPrice() {
+
+        return null;
+    }
+
+    private ArrayList<Vehicle> getVehiclesByMakeModel() {
+
+        return null;
+    }
+
+    private ArrayList<Vehicle> getVehiclesByYear() {
+
+        return null;
+    }
+
+    private ArrayList<Vehicle> getVehiclesByColor() {
+
+        return null;
+    }
+
+    private ArrayList<Vehicle> getVehiclesByMileage() {
+
+        return null;
+    }
+
+    private ArrayList<Vehicle> getVehiclesByType() {
+        return null;
+    }
+
+    private void addVehicle() {
+
+    }
+
+    private void removeVehicle() {
+
+    }
+
+
+}
